32c6fb28237d5fabed8e25bfcfb2942bad0f52a6
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2025-01-16 21:23:13 +0000

    create the record Calendary and introduce the dependecy

---
+++ src/main/java/software/ulpgc/Calendary.java
@@ -0,0 +1,29 @@
+package software.ulpgc;
+
+import java.time.LocalDate;
+import java.util.Iterator;
+
+public record Calendary() {
+    public Iterable<LocalDate> getDaysFrom(LocalDate from) {
+        return new Iterable<LocalDate>() {
+            @Override
+            public Iterator<LocalDate> iterator() {
+                return new Iterator<LocalDate>() {
+                    @Override
+                    public boolean hasNext() {
+                        return true;
+                    }
+
+                    @Override
+                    public LocalDate next() {
+                        LocalDate next = current;
+                        current = next.plusDays(1);
+                        return next;
+                    }
+
+                    private LocalDate current = from;
+                };
+            }
+        };
+    }
+}
