b9e97eb59e417587649be323a9ffd7df77745c5d
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2025-01-16 22:51:18 +0000

    create the class CommandFactory

---
+++ src/main/java/software/ulpgc/CommandFactory.java
@@ -0,0 +1,25 @@
+package software.ulpgc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandFactory {
+    private final Map<String, CommandBuilder> commandBuilderMap;
+
+    public CommandFactory() {
+        commandBuilderMap = new HashMap<>();
+    }
+
+    public CommandFactory register(String key, CommandBuilder value) {
+        commandBuilderMap.put(key, value);
+        return this;
+    }
+
+    public Command get(String key, Input input, Output output) {
+        return commandBuilderMap.get(key).build(input, output);
+    }
+
+    public interface CommandBuilder {
+        Command build(Input input, Output output);
+    }
+}
